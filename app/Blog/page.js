6685a2ecc75c2bfe2f84149bafabfb55273c3bfe
saga-r-code <shah.sagar2006@gmail.com>
2024-07-29 03:31:08 +0530

    slug and create blog page

--- app/Blog/page.js
+++ app/Blog/page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '@/Components/Navbar'
 import Image from '@/Components/Image'
+import Link from 'next/link'
 
 
 const Blog = () => {
@@ -8,7 +9,15 @@ const Blog = () => {
     <div>
       <Image />
       <Navbar />  
-      This Blog page
+      <div className="popular-blog my-16 flex justify-center items-center flex-col text-white">
+          <h2 className="text-3xl font-bold text-slate-300 ">Popular Blog</h2>
+          <div className="blog m-7 flex flex-col gap-2">
+            <Link href={'/learn-javascript'}><h3 className="text-xl font-semibold">How to learn JavaScript in 2024</h3></Link>
+            <p className="text-slate-400">
+              JavaScript is the language used to design logic for the web page
+            </p>
+          </div>
+        </div>
     </div>
   )
 }
---
+++ app/[slug]/page.js
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+import { useParams } from "next/navigation";
+import Image from "@/Components/Image";
+import Navbar from '@/Components/Navbar'
+
+const Slug = () => {
+  const params = useParams();
+  const slug = params.slug; // Ensure correct parameter access
+
+  return (
+    <>
+      <Image />
+      <Navbar />
+      <div className="javascript-page  w-9/12  mx-auto" >
+      <div className="text-center py-16 text-white  gap-5 flex justify-center items-center flex-col">
+      <h1 className="text-4xl font-semibold ">Tiltle of  {slug}</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, aliquid non! Obcaecati, sapiente sunt quo ullam eos, minima consequuntur ea voluptas ab alias inventore quas explicabo! Enim voluptas fugiat molestiae.</p>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default Slug;
